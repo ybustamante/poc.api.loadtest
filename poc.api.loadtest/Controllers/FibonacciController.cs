552664c104e44b00d371ca80195e5af3ea2c1531
ybustamante <Think*25>
2021-07-08 19:40:42 -0400

    Add project files.

---
+++ poc.api.loadtest/Controllers/FibonacciController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace poc.api.loadtest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FibonacciController : ControllerBase
+    {
+        [HttpGet]
+        public int Get(int position)
+        {
+
+            return CalculateFibonacci(position);
+        }
+
+        private int CalculateFibonacci(int position)
+        {
+            if (position == 0 || position == 1)
+            {
+                return position;
+            }           
+            else
+            {
+                return CalculateFibonacci(position - 1) + CalculateFibonacci(position - 2);
+            }
+        }
+
+    }
+}
