52c668c252b9537b9fe472748034ea4116ca2224
ybustamante <Think*25>
2021-10-06 23:03:40 -0300

    Docker file with ingress nginx

--- poc.api.loadtest.test/FibonacciController.cs
+++ poc.api.loadtest.test/FibonacciController.cs
@@ -4,6 +4,7 @@ namespace poc.api.loadtest.test
 {
     public class FibonacciController
     {
+
         [SetUp]
         public void Setup()
         {
@@ -12,6 +13,17 @@ namespace poc.api.loadtest.test
         [Test]
         public void Fibonacci()
         {
+
+            var service = new Mock<IService>();
+            controller = new MyController(service.Object);
+
+            service.Setup(service => service.GetAsync(1)).ReturnsAsync((MyType)null);
+
+            var result = await controller.Get(1);
+
+            Assert.That(result.Result, Is.TypeOf<NotFoundResult>());
+
+
             var calculatePosition = FibonacciController.CalculateFibonacci(2);
             Assert.Pass();
         }
